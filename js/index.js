131014ca66df9ab1684cd479b0acab8401d3c45e
Ignacionande98 <128426701+Ignacionande98@users.noreply.github.com>
2023-09-07 17:51:43 -0300

    cambios en el codigo

--- js/index.js
+++ js/index.js
@@ -1,31 +1,7 @@
 const url = "https://pokeapi.co/api/v2/pokemon";
 const listaPokemon = document.querySelector("#lista-pokemon");
 
-// Funcion para realizar la solicitud al servidor y convertir a formato JSON
-function obtenerPokemon(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((pokemonData) => {
-      mostrarPokemon(pokemonData);
-    })
-    .catch((error) => console.error("Error al cargar los datos del Pokémon:", error));
-}
-
-
-// Funcion para obtener la lista de Pokemon
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const pokemonLista = data.results
-    listaPokemon.innerHTML = ""; 
-    pokemonLista.forEach((pokemon) => {
-      obtenerPokemon(pokemon.url);
-    });
-  })
-  .catch((error) => console.error("Error al cargar la lista de Pokémon:", error));
-
-  
-// Función para mostrar los Pokemon en la lista
+// funcion para cargar los pokemon en la lista
 function mostrarPokemon(pokemonData) {
   const listItem = document.createElement("li");
   listItem.innerHTML = `
@@ -34,3 +10,31 @@ function mostrarPokemon(pokemonData) {
   `;
   listaPokemon.appendChild(listItem);
 }
+
+// funcion para obtener los pokemon y mostrarlos
+function obtenerListaPokemon() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemonLista = data.results;
+      listaPokemon.innerHTML = "";
+      pokemonLista.forEach((pokemon) => {
+        obtenerPokemon(pokemon.url);
+      });
+    })
+    .catch((error) => console.error("Error al cargar la lista de Pokémon:", error));
+}
+
+
+// realizamos solicitud al servidor y convertimos en formato JSON
+function obtenerPokemon(pokemonUrl) {
+  fetch(pokemonUrl)
+    .then((response) => response.json())
+    .then((pokemonData) => {
+      mostrarPokemon(pokemonData);
+    })
+    .catch((error) => console.error("Error al cargar los datos del Pokémon:", error));
+}
+
+
+obtenerListaPokemon();
